34250bf05a15872bbb5df432fa59a607d2ac1345
ironsj <jakeirons007@gmail.com>
2023-03-04 01:09:08 -0500

    Locations of wikipedia articles now show up based on region of map

--- WikiNearMe/App.tsx
+++ WikiNearMe/App.tsx
@@ -2,12 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker, Region } from 'react-native-maps';
 import axios, { AxiosResponse } from 'axios';
+import { Article } from './datatypes';
+import { SetStateAction, useState } from 'react';
 
 export default function App() {
   let centralCoordinates = {
     latitude: '',
     longitude: '',
   }
+
+  let [articles, setArticles] = useState(Array<Article>());
+
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  });
 
   function getCentralCoordinates(region: Region) {
     console.log(region.latitude, region.longitude)
@@ -17,20 +28,29 @@ export default function App() {
     };
   }
 
-  const findArticles = (coordinates: any) => {
+  const findArticles = () => {
     let centralLat: string = centralCoordinates.latitude;
     let centralLong: string = centralCoordinates.longitude;
-    let searchurl: string = ''.concat('https://en.wikipedia.org/w/api.php?action=query&gscoord=', centralLat, '|', centralLong, '&gslimit=1&gsradius=10000&list=geosearch&format=json');
+    let searchurl: string = ''.concat('https://en.wikipedia.org/w/api.php?action=query&gscoord=', centralLat, '|', centralLong, '&gslimit=50&gsradius=10000&list=geosearch&format=json');
+
+    console.log(searchurl);
 
     axios.request({
       url: searchurl,
       method: 'GET',
-    }).then((response: AxiosResponse) => {
-      console.log(response.data);
-    }).catch((error) => {
-      console.log(error);
-    });
+    }).then((response: AxiosResponse) => response.data.query.geosearch)
+      .then((articleData: any) => {
+        articles.splice(0);
+        let newArticles = [];
+        newArticles.push(...articleData);
+        setArticles(newArticles);
+        console.log(articles)
+      });
   }
+
+  const onRegionChangeComplete = (newRegion: SetStateAction<{ latitude: number; longitude: number; latitudeDelta: number; longitudeDelta: number; }>) => {
+    setRegion(newRegion);
+  };
 
   return (
     <View style={styles.container}>
@@ -50,20 +70,18 @@ export default function App() {
           console.log(centralCoordinates)
         }}
         onRegionChangeComplete={() => {
-          findArticles(centralCoordinates)
+          findArticles()
+          onRegionChangeComplete
         }}
       >
-        <Marker
-          onPress={() => {
-            getCentralCoordinates;
-          }}
-          coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-          }}
-          title="My Marker"
-          description="Some description"
-        />
+        {articles.map((e: Article) => (
+          <Marker
+            key={e.title}
+            coordinate={{ latitude: e.lat, longitude: e.lon }}
+            title={e.title}
+            description={e.title}
+          />
+        ))}
       </MapView>
     </View>
   );
